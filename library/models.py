3458031f51f79fc8dd304566c6bf272342c875d3
ali8bits <the.ali8bits@gmail.com>
2024-08-14 15:26:22 -0700

    feat: add Book Model.

--- library/models.py
+++ library/models.py
@@ -12,3 +12,21 @@ class Category(models.Model):
     class Meta:
         verbose_name = "دسته بندی"
         verbose_name_plural = "دسته بندی ها"
+
+
+class Book(TimeStampedAbstractModel, models.Model):
+    title = models.CharField(max_length=255, verbose_name="عنوان کتاب")
+    author = models.CharField(max_length=255, verbose_name="نویسنده")
+    translator = models.CharField(max_length=255, blank=True, null=True, verbose_name="مترجم")
+    publisher = models.CharField(max_length=255, verbose_name="انتشارات")
+    volume_number = models.IntegerField(verbose_name="شماره جلد")
+    publication_count = models.IntegerField(verbose_name="سال انتشار")
+    page_count = models.IntegerField(verbose_name="تعداد صفحه")
+    owner = models.CharField(max_length=255, verbose_name="صاحب کتاب")
+    description = models.TextField(verbose_name="توضیحات")
+    count = models.IntegerField(verbose_name="تعداد موجودی")
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, related_name="books", verbose_name="دسته بندی")
+
+    class Meta:
+        verbose_name = "کتاب"
+        verbose_name_plural = "کتاب ها"
