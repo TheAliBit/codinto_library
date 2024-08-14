31dd27dec0fc19f4d67cf3fd61ddad2a66cd8351
ali8bits <the.ali8bits@gmail.com>
2024-08-14 02:56:52 -0700

    feat: add Category model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.models import TimeStampedAbstractModel
+
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255, unique=True, verbose_name="عنوان دسته بندی")
+    image = models.ImageField(upload_to='uploads/', verbose_name="تصویر دسته بندی")
+    parent = models.ForeignKey('Category', blank=True, null=True, on_delete=models.SET_NULL, related_name='children',
+                               verbose_name="دسته بندی پدر")
+
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
