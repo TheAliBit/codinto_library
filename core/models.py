f9e1ce5e441640201bfd2da35d6d1c635f9f0a89
ali8bits <the.ali8bits@gmail.com>
2024-08-14 02:00:39 -0700

    feat: add Profile model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class Profile(AbstractUser):
+    username = models.CharField(max_length=255, unique=True, verbose_name="نام کاربری")
+    password = models.CharField(max_length=255, verbose_name="رمزعبور")
+    first_name = models.CharField(max_length=255, verbose_name="نام")
+    last_name = models.CharField(max_length=255, verbose_nam="نام خانوادگی")
+    phone_number = models.CharField(max_length=255, verbose_name="شماره تلفن")
+    email = models.CharField(max_length=255, verbose_name="ایمیل")
+    telegram_id = models.CharField(max_length=255, verbose_name="آیدی تلگرام")
+    picture = models.ImageField(upload_to='uploads/', verobse_name='آواتار')
