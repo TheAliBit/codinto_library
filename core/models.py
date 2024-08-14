b2d99050a9ea38d69bb61f486e576a82a7dc3b6a
ali8bits <the.ali8bits@gmail.com>
2024-08-14 02:33:51 -0700

    feat: add TimeStampedAbstractModel

--- core/models.py
+++ core/models.py
@@ -8,8 +8,20 @@ class Profile(AbstractUser):
     username = models.CharField(max_length=255, unique=True, verbose_name="نام کاربری")
     password = models.CharField(max_length=255, verbose_name="رمزعبور")
     first_name = models.CharField(max_length=255, verbose_name="نام")
-    last_name = models.CharField(max_length=255, verbose_nam="نام خانوادگی")
+    last_name = models.CharField(max_length=255, verbose_name="نام خانوادگی")
     phone_number = models.CharField(max_length=255, verbose_name="شماره تلفن")
-    email = models.CharField(max_length=255, verbose_name="ایمیل")
+    email = models.EmailField(max_length=255, verbose_name="ایمیل")
     telegram_id = models.CharField(max_length=255, verbose_name="آیدی تلگرام")
-    picture = models.ImageField(upload_to='uploads/', verobse_name='آواتار')
+    picture = models.ImageField(upload_to='uploads/', verbose_name='آواتار')
+
+    class Meta:
+        verbose_name = "پروفایل"
+        verbose_name_plural = "پروفایل ها"
+
+
+class TimeStampedAbstractModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
