34aef506ff4f8fbc3b6b2148b03882e3eaa4c2b7
ali8bits <the.ali8bits@gmail.com>
2024-10-05 05:09:25 -0700

    feat: add pytest for super user request and super user single request

--- library/tests/test_user_urls.py
+++ library/tests/test_user_urls.py
@@ -3,6 +3,7 @@ from rest_framework import status
 import pytest
 from rest_framework.status import HTTP_401_UNAUTHORIZED
 
+from core.models import Profile
 from library.models import ReviewRequest, Book, BorrowRequest
 
 
@@ -140,6 +141,22 @@ def super_user_get_requests(api_client):
         return api_client.get('/super-user/requests/')
 
     return get_requests
+
+
+@pytest.fixture
+def super_user_get_single_request(api_client):
+    def get_single_request(request_id):
+        return api_client.get(f'/super-user/requests/{request_id}/')
+
+    return get_single_request
+
+
+@pytest.fixture
+def super_user_update_single_request(api_client):
+    def update_single_request(request_id, data):
+        return api_client.put(f'/super-user/requests/{request_id}/', data=data)
+
+    return update_single_request
 
 
 @pytest.mark.django_db
@@ -504,3 +521,76 @@ class TestGetSuperUserRequests:
         response = super_user_get_requests()
 
         assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+class TestGetSuperUserSingleRequest:
+    def test_if_user_is_annonymous_returns_401(self, super_user_get_single_request):
+        response = super_user_get_single_request(request_id=1)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, user, api_client, super_user_get_single_request):
+        api_client.force_authenticate(user=user)
+        response = super_user_get_single_request(request_id=1)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_is_admin_returns_200(self, staff_user, api_client, super_user_get_single_request):
+        api_client.force_authenticate(user=staff_user)
+        user = baker.make(Profile)
+        book = baker.make(Book)
+        request = baker.make(BorrowRequest, user=user, book=book, time=14, status='accepted')
+        response = super_user_get_single_request(request_id=request.id)
+
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_user_is_admin_but_request_does_not_exists_returns_404(self, staff_user, api_client,
+                                                                      super_user_get_single_request):
+        api_client.force_authenticate(user=staff_user)
+        response = super_user_get_single_request(request_id=1000)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+class TestUpdateSuperUserRequest:
+    def test_if_user_is_annonymous_returns_401(self, super_user_update_single_request):
+        response = super_user_update_single_request(request_id=1, data={})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, user, api_client, super_user_update_single_request):
+        api_client.force_authenticate(user=user)
+        response = super_user_update_single_request(request_id=1, data={})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_is_admin_returns_200(self, staff_user, api_client, super_user_update_single_request):
+        api_client.force_authenticate(user=staff_user)
+        book = baker.make(Book)
+        user = baker.make(Profile)
+        request = baker.make(BorrowRequest, user=user, book=book, time=14)
+
+        updated_data = {
+            'status': 'accepted'
+        }
+
+        response = super_user_update_single_request(request_id=request.id, data=updated_data)
+
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_user_is_admin_but_data_is_invalid_returns_400(self, staff_user, api_client,
+                                                              super_user_update_single_request):
+        api_client.force_authenticate(user=staff_user)
+        book = baker.make(Book)
+        user = baker.make(Profile)
+        request = baker.make(BorrowRequest, user=user, book=book, time=14)
+
+        updated_data = {
+            'status': 'iosadhgjksa'
+        }
+
+        response = super_user_update_single_request(request_id=request.id, data=updated_data)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
