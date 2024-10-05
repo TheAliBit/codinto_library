bcb78d6f29509811da4e546b4e6a5d5f20cf7b68
ali8bits <the.ali8bits@gmail.com>
2024-10-05 00:23:52 -0700

    feat: add pytest to test single user update and delete

--- core/tests/test_profile.py
+++ core/tests/test_profile.py
@@ -46,6 +46,14 @@ def super_user_delete_single_user(api_client):
     return delete_user
 
 
+@pytest.fixture
+def super_user_update_single_user(api_client):
+    def update_user(user_id, data):
+        return api_client.put(f'/super-user/users/{user_id}/', data=data)
+
+    return update_user
+
+
 @pytest.mark.django_db
 class TestCreateProfile:
     def test_if_user_is_anonymous_returns_401(self, api_client, super_user_create):
@@ -104,6 +112,7 @@ class TestGetProfile:
 class TestGetInSuperUserSearch:
     def test_if_user_is_anonymous_returns_401(self, super_user_search):
         response = super_user_search()
+
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_if_user_is_not_admin_returns_403(self, api_client, user, super_user_search):
@@ -115,7 +124,9 @@ class TestGetInSuperUserSearch:
 
     def test_if_user_is_admin_returns_200(self, api_client, super_user_search, staff_user):
         api_client.force_authenticate(user=staff_user)
+
         response = super_user_search()
+
         assert response.status_code == status.HTTP_200_OK
 
 
@@ -123,18 +134,22 @@ class TestGetInSuperUserSearch:
 class TestRetrieveUserDetail:
     def test_if_user_is_anonymous_returns_401(self, super_user_get_single_user):
         response = super_user_get_single_user(user_id=1)
+
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_if_user_is_not_admin_returns_403(self, api_client, user, super_user_get_single_user):
         api_client.force_authenticate(user=user)
         response = super_user_get_single_user(user_id=1)
+
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
     def test_if_user_is_admin_returns_200(self, api_client, staff_user, super_user_get_single_user):
         api_client.force_authenticate(user=staff_user)
         user = baker.make(Profile)
         response = super_user_get_single_user(user_id=user.id)
+
         assert response.status_code == status.HTTP_200_OK
+
         assert response.data['id'] == user.id
 
 
@@ -142,16 +157,83 @@ class TestRetrieveUserDetail:
 class TestDeleteUser:
     def test_if_user_is_anonymous_returns_401(self, super_user_delete_single_user):
         response = super_user_delete_single_user(user_id=1)
+
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_if_user_is_not_admin_returns_403(self, api_client, user, super_user_delete_single_user):
         api_client.force_authenticate(user=user)
         response = super_user_delete_single_user(user_id=1)
+
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_user_is_admin_deletes_profile_returns_200(self, api_client, staff_user, super_user_delete_single_user):
+    def test_if_user_is_admin_deletes_profile_returns_204(self, api_client, staff_user, super_user_delete_single_user):
         api_client.force_authenticate(user=staff_user)
         user = baker.make(Profile)
         response = super_user_delete_single_user(user_id=user.id)
+
         assert response.status_code == status.HTTP_204_NO_CONTENT
+
         assert Profile.objects.filter(id=user.id).exists() is False
+
+    def test_if_user_does_not_exist_returns_404(self, api_client, staff_user, super_user_delete_single_user):
+        api_client.force_authenticate(user=staff_user)
+        response = super_user_delete_single_user(user_id=999)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+class TestUpdateUser:
+    def test_if_user_is_anonymous_returns_401(self, super_user_update_single_user):
+        response = super_user_update_single_user(user_id=1, data={})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self, api_client, user, super_user_update_single_user):
+        api_client.force_authenticate(user=user)
+
+        response = super_user_update_single_user(user_id=1, data={})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_user_is_admin_update_and_returns_200(self, api_client, staff_user, super_user_update_single_user):
+        api_client.force_authenticate(user=staff_user)
+
+        user = baker.make(Profile)
+
+        updated_data = {
+            'username': 'test',
+            'password': 'testpassword',
+            'first_name': 'jamshit',
+            'last_name': 'jamshiti',
+            'email': 'test@gmail.com',
+            'telegram_id': 'test_id',
+            'phone_number': '09390605460',
+        }
+
+        response = super_user_update_single_user(user_id=user.id, data=updated_data)
+        print(response.data)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_if_user_is_admin_but_data_is_bad_returns_400(self, api_client, staff_user, super_user_update_single_user):
+        api_client.force_authenticate(user=staff_user)
+        user = baker.make(Profile)
+
+        updated_data = {
+            'username': user.id,
+            'password': 'testpassword',
+            'first_name': 'jamshit',
+            'last_name': 'jamshiti',
+            'email': '',
+            'telegram_id': '',
+            'phone_number': '',
+        }
+
+        response = super_user_update_single_user(user_id=user.id, data=updated_data)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_if_user_does_not_exists_returns404(self, staff_user, api_client, super_user_update_single_user):
+        api_client.force_authenticate(user=staff_user)
+        response = super_user_update_single_user(user_id=999, data={})
+        assert response.status_code == status.HTTP_404_NOT_FOUND
