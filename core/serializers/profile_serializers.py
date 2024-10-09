cdda72488ba2d686b3a7c5a0312608bd9a8bfeb9
ali8bits <the.ali8bits@gmail.com>
2024-10-09 02:51:51 -0700

    ref: delete duplicated code in serializer

--- core/serializers/profile_serializers.py
+++ core/serializers/profile_serializers.py
@@ -45,20 +45,6 @@ class ProfileSerializer(serializers.ModelSerializer):
             instance.save()
         return instance
 
-    def create(self, validated_data):
-        passwrod = validated_data.pop('password')
-        user = Profile(**validated_data)
-        user.set_password(passwrod)
-        user.save()
-        return user
-
-    def update(self, instance, validated_data):
-        password = validated_data.pop('password', None)
-        instance = super().update(instance, validated_data)
-        if password:
-            instance.set_password(password)
-            instance.save()
-        return instance
 
 
 class AdminListProfileSerializer(serializers.ModelSerializer):
