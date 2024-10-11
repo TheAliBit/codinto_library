f93141f7d144343ef9f719f9bff36cf9619a5fad
TheAliBit <the.ali8bits@gmail.com>
2024-10-11 14:41:28 -0700

    feat: change the celery setting

--- codinto_library/celery_settings.py
+++ codinto_library/celery_settings.py
@@ -4,6 +4,6 @@ CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BEAT_SCHEDULE = {
     'check_legal_borrow_date': {
         'task': 'library.tasks.check_legal_borrow_date',
-        'schedule': 10,  # This runs every day at midnight (00:00)
+        'schedule':  crontab(hour=0, minute=0),  # This runs every day at midnight (00:00)
     }
 }
